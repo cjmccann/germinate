82b5980d83a2710a6b39b00c731162a0a246446f
cjmccann <connor.j.mccann@gmail.com>
2014-05-04 14:07:16 -0400

    working scripts

---
+++ Assets/Data.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var barDisplay : float = 0;
+var pos : Vector2 = new Vector2(10, 40);
+var size : Vector2 = new Vector2(100, 20);
+var progressBarEmpty : Texture2D;
+var progressBarFull : Texture2D;
+
+function OnGUI() {
+	GUI.Label(Rect(10, 5, Screen.width, Screen.height), "Time: " + Time.time.ToString());
+
+	GUI.BeginGroup(new Rect (pos.x, pos.y, size.x, size.y));
+		GUI.Box (Rect (0, 0, size.x, size.y), progressBarEmpty);
+	
+		GUI.BeginGroup (new Rect(0, 0, size.x * barDisplay, size.y));
+			GUI.Box (Rect (0, 0, size.x, size.y), progressBarFull);
+		GUI.EndGroup();
+	GUI.EndGroup();
+}
+
+function Start () {
+
+}
+
+function Update () {
+	barDisplay = Time.time * 0.05;
+}
--- Assets/growRoot.js
+++ Assets/growRoot.js
@@ -2,8 +2,8 @@
 
 public var rootPrefab : GameObject;
 public var roots : int;
-public var rootArray = [];
-public 
+public var rootArray : GameObject[];
+private var index = 0;
 
 function Start () {	
 	roots = 1;
@@ -22,50 +22,49 @@ function Update () {
 			var newVector : Vector2;
 			
 			if (roots > 1) {
+				rootArray = gameObject.FindGameObjectsWithTag("Root");
 				while (true) {
-					var rand = Random.Range(0, roots);
-					var rootSelection = rootArray[rand].GetComponent("rootClass");
-					if (rootSelection.full) { }
+					var rand = Random.Range(0, rootArray.Length);
+					var rootSelection : rootClass = rootArray[rand].GetComponent(rootClass);
+					if (rootSelection.rootLeft && rootSelection.rootRight && rootSelection.rootCenter) { }
 					else {
 						newRoot = Instantiate(rootPrefab);
 						while (true) {
 							var side = Random.Range(0, 3);
 							if (side == 0 && !rootSelection.rootLeft) {
-								newVector = Vector2(-.2903818, -0.2403101);
-								rootArray.push(newRoot);
-								roots++;
+								newRoot.transform.parent = rootArray[rand].transform;
+								newRoot.transform.localPosition = Vector2(-0.2906262, -0.236388);
 								
-								newRoot.transform.parent = rootArray[rand];
-								newRoot.transform.position = newVector;
+								rootSelection.rootLeft = true;
 								break;
 							} else if (side == 1 && !rootSelection.rootCenter) {
-								newVector = Vector2(-.03976905, -0.2403101);
-								rootArray.push(newRoot);
-								roots++;
+								newRoot.transform.parent = rootArray[rand].transform;
+								newRoot.transform.localPosition = Vector2(-.03976905, -0.2403101);
 								
-								newRoot.transform.parent = rootArray[rand];
-								newRoot.transform.position = newVector;
+								rootSelection.rootCenter = true;
 								break;
-							} else if (side == 2 && !rootSelection.rootCenter) {
-								newVector = Vector2(.2805354, -0.2403101);
-								rootArray.push(newRoot);
-								roots++;
+							} else if (side == 2 && !rootSelection.rootRight) {
+								newRoot.transform.parent = rootArray[rand].transform;
+								newRoot.transform.localPosition = Vector2(.2805354, -0.2403101);
 								
-								newRoot.transform.parent = rootArray[rand];
-								newRoot.transform.position = newVector;
+								rootSelection.rootRight = true;
 								break;
 							}
 						}
+						newRoot.gameObject.tag="Root";
+						index++;
+						roots++;
 						timer = 0.0;
 						break;
 					}
 				}
 			} else {
 				newRoot = Instantiate(rootPrefab);
-				newVector = Vector2(.02620226, -.3690214);
-				rootArray.push(newRoot);
+				newVector = Vector2(0.1011564, -1.219302);
 				roots++;
+				index++;
 				
+				newRoot.gameObject.tag="Root";
 				newRoot.transform.parent = transform;
 				newRoot.transform.position = newVector;
 				timer = 0.0;
